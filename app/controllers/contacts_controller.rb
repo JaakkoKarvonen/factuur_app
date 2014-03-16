bd4468e9c0440434cc531a873bb6158e6b803ff0
Jaakko Karvonen <jaakkokarvonen@hotmail.com>
2014-03-16 16:46:18 +0100

    css added contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:notice] = "Contact is succesvol toegevoegd!"
-      redirect_to @contact
+      redirect_to "/contacts"
     else
       render 'new'
     end
@@ -32,7 +32,7 @@ class ContactsController < ApplicationController
     @contact = Contact.find_by_id(params[:id])
     if @contact.update_attributes(contact_params)
       flash[:notice] = "Contact gewijzigd!"
-      redirect_to @contact
+      redirect_to "/contacts"
     else
       render 'edit'
     end
