0e533fa267e9ef198c1b3fc6c2c37bcaaa9692b5
Jaakko Karvonen <jaakkokarvonen@hotmail.com>
2014-03-25 14:52:28 +0100

    dus

--- db/migrate/20140325134714_create_products.rb
+++ db/migrate/20140325134714_create_products.rb
@@ -1,10 +1,11 @@
 class CreateProducts < ActiveRecord::Migration
   def change
+
     create_table :products do |t|
     	t.string :name
       	t.string :description
       	t.string :serial
-      	t.integer :price
+      	t.decimal :price, :precision => 8, :scale => 2
 
       	t.timestamps
     end
